836b33a5fc71fe485a5ed5a1f020adf115e102b7
Stefan <stefigabi89@yahoo.com>
2024-05-06 13:20:56 +0200

    Create a card to select and choose and a table to show the data

--- BlazorServerApp/TodoItem.cs
+++ BlazorServerApp/TodoItem.cs
@@ -5,5 +5,9 @@
         public string? Title { get; set; }
         public bool IsDone { get; set; } = false;
         public string Task { get; set; }
+
+        public DateTime? DueDate { get; set; }
+        public string Priority { get; set; } = "Normal";
+        public TimeOnly? DueTime { get; set; }
     }
 }
